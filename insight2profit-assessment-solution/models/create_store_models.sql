5c36e30dd90a9557ca7f6baf65d2abb0d827cb12
Joshua Gbegudu <jk.gbegudu@gmail.com>
2026-02-26 11:44:45 -0500

    Create sales order header and detail tables
    Added SQL statements to create sales order header and detail tables.

--- insight2profit-assessment-solution/models/create_store_models.sql
+++ insight2profit-assessment-solution/models/create_store_models.sql
@@ -18,4 +18,33 @@ SELECT
     CAST(WeightUnitMeasureCode AS CHAR(3)) AS weight_unit_measure_code,
     CAST(ProductCategoryName AS VARCHAR(50)) AS product_category_name,
     CAST(ProductSubCategoryName AS VARCHAR(50)) AS product_sub_category_name
-FROM raw.raw_product
+FROM raw.raw_product;
+
+--Sales Order Header
+CREATE TABLE store.store_sales_order_header AS
+SELECT
+    CAST(NULLIF(SalesOrderID, '') AS INT) AS sales_order_id,
+    CAST(
+        CASE 
+            WHEN LENGTH(NULLIF(orderdate, '')) = 7 THEN orderdate || '-01'
+            ELSE NULLIF(orderdate, '')
+        END AS DATE
+    ) AS order_date,
+    CAST(NULLIF(ShipDate, '') AS DATE) AS ship_date,
+    CAST(NULLIF(OnlineOrderFlag, '') AS BOOLEAN) AS online_order_flag,
+    NULLIF(AccountNumber, '') AS account_number,
+    CAST(NULLIF(CustomerID, '') AS INT) AS customer_id,
+    CAST(NULLIF(SalesPersonID, '') AS INT) AS sales_person_id,
+    CAST(NULLIF(Freight, '') AS NUMERIC(12,2)) AS freight
+FROM raw.raw_sales_order_header;
+
+--Sales Order Detail
+CREATE TABLE store.store_sales_order_detail AS
+SELECT
+    CAST(NULLIF(SalesOrderID, '') AS INT) AS sales_order_id,
+    CAST(NULLIF(SalesOrderDetailID, '') AS INT) AS sales_order_detail_id,
+	CAST(NULLIF(OrderQty, '') AS INT) AS order_qty,
+    CAST(NULLIF(ProductID, '') AS INT) AS product_id,
+    CAST(NULLIF(UnitPrice, '') AS NUMERIC(12,2)) AS unit_price,
+    CAST(NULLIF(UnitPriceDiscount, '') AS NUMERIC(12,2)) AS unit_price_discount
+FROM raw.raw_sales_order_detail;
